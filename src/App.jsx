2c4dd4698e57a8e7fe654ff0c0cb10cd15d4b988
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-04-06 21:48:18 +0200

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import MainContent from './components/MainContent'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <Navbar></Navbar>
+      <MainContent></MainContent>
+    </div>
+  )
+}
+
+export default App
